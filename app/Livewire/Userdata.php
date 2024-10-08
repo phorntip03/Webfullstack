0b4b7d73fb47f50ed06e95f61f82f44806f68966
phorntip03 <oopontip7524@gmail.com>
2024-10-08 14:24:07 +0700

    up

--- app/Livewire/Userdata.php
+++ app/Livewire/Userdata.php
@@ -3,11 +3,15 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\models\User;
 
 class Userdata extends Component
 {
     public function render()
     {
-        return view('livewire.userdata');
+        //ดึงข้อมูลทั้งหมดจาก User เก็บใส่ตัวแปร data
+        $data = User::all();
+         //withcompact คือทุกครั้งที่มีการ rander ใหม่ ให้ส่งตัวแปร data ไปที่หน้า view ด้วย
+        return view('livewire.userdata')->with(compact('data'));
     }
 }
